ed529178c5c5a9eac4df0f2f6bcd10b5d20a965d
deslobodzian <de.slobodzian@gmail.com>
2022-04-13 10:01:15 -0400

    Fixing drive command.

--- src/main/java/org/frc5687/swerve/commands/Drive.java
+++ src/main/java/org/frc5687/swerve/commands/Drive.java
@@ -1,46 +1,50 @@
 /* Team 5687 (C)2021-2022 */
 package org.frc5687.swerve.commands;
 
+import static org.frc5687.swerve.Constants.DriveTrain.*;
+
 import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.math.geometry.Rotation2d;
 import org.frc5687.swerve.OI;
 import org.frc5687.swerve.subsystems.DriveTrain;
 
 public class Drive extends OutliersCommand {
 
     private final DriveTrain _driveTrain;
+    private final SlewRateLimiter _vxFilter;
+    private final SlewRateLimiter _vyFilter;
 
     private final OI _oi;
-    private final SlewRateLimiter _vxLimiter;
-    private final SlewRateLimiter _vyLimiter;
 
     public Drive(DriveTrain driveTrain, OI oi) {
         _driveTrain = driveTrain;
         _oi = oi;
-        _vxLimiter = new SlewRateLimiter(4); // units per sec
-        _vyLimiter = new SlewRateLimiter(4); // units per sec
+        _vxFilter = new SlewRateLimiter(4.0);
+        _vyFilter = new SlewRateLimiter(4.0);
         addRequirements(_driveTrain);
+        //        logMetrics("vx","vy");
+        //        enableMetrics();
     }
 
     @Override
     public void initialize() {
-        super.initialize();
         _driveTrain.startModules();
+        _driveTrain.setFieldRelative(true);
+        _driveTrain.setControlState(DriveTrain.ControlState.MANUAL);
     }
 
     @Override
     public void execute() {
-        super.execute();
         //  driveX and driveY are swapped due to coordinate system that WPILib uses.
-        double x = _vxLimiter.calculate(_oi.getDriveY());
-        double y = _vyLimiter.calculate(_oi.getDriveX());
-        double omega = _oi.getRotationX();
-        if (_oi.snap()) {
-            _driveTrain.setSnapHeading(new Rotation2d(Math.PI / 2.0));
-        } else if (_oi.autoAim()) {
+        double vx = _vxFilter.calculate(_oi.getDriveY());
+        double vy = _vyFilter.calculate(_oi.getDriveX());
+        double rot = _oi.getRotationX();
+
+        if (_oi.autoAim()) {
             _driveTrain.vision(_driveTrain.getVisionHeading());
+            rot = 0.0;
         }
-        _driveTrain.drive(x, y, omega, true);
+
+        _driveTrain.drive(vx, vy, rot);
     }
 
     @Override
