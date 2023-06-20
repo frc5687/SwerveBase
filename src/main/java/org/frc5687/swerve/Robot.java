59335c77defea883925fb36ece22d032e6ee0c0c
SuperSaurus1 <78873930+SuperSaurus1@users.noreply.github.com>
2023-06-20 18:23:05 -0400

    Cleaning yellow lines :)

--- src/main/java/org/frc5687/swerve/Robot.java
+++ src/main/java/org/frc5687/swerve/Robot.java
@@ -61,7 +61,7 @@ public class Robot extends OutliersRobot implements ILoggingSource {
         // Periodically flushes metrics (might be good to configure enable/disable via USB config
         // file)
         _time = _timer.get();
-        new Notifier(MetricTracker::flushAll).startPeriodic(Constants.METRIC_FLUSH_PERIOD);
+        // new Notifier(MetricTracker::flushAll).startPeriodic(Constants.METRIC_FLUSH_PERIOD);
     }
 
     /**
